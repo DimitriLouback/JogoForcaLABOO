129efad94fa2b767465c61d27407c22a3acbfbab
Dimitri <dimitrilouback91@gmail.com>
2023-09-16 20:32:21 -0300

    palavra

--- palavra.java
+++ palavra.java
@@ -2,6 +2,11 @@ public class palavra {
 
     public static void main(String[] args) {
         System.out.println("palavra");
+
+        String palavra = " ";
+        if(palavra == "aa") {
+            System.out.println("oioi");
+        }
     }
     
 }
